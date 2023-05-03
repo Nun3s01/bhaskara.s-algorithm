b4129b32c51d9a00914b06d70026e1b577464089
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2023-05-03 20:16:00 -0300

    Bhaskara's algorithm.py

---
+++ Bhaskara's algorithm.py
@@ -0,0 +1,21 @@
+#   Quadratic algorithm
+import cmath, math;
+valueA = float(input("What is the value of 'a'? "))
+valueB = float(input("What is the value of 'b'? "))
+valueC = float(input("What is the value of 'c'? "))
+
+delta = (valueB**2)-(4*valueA*valueC)
+
+if (delta == 0):                                                                   
+    x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))
+    print(f"Δ = {int(delta)} \nWith the only real root being: {x1}")
+else:
+    if (delta < 0):
+        delta = (valueB**2)-(4*valueA*valueC)
+        x1 = (-valueB + cmath.sqrt(delta)) / (2*valueA)
+        x2 = (-valueB - cmath.sqrt(delta)) / (2*valueA)
+        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2} \n(Δ < 0 there are no real roots)")
+    else:
+        x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))                                                  
+        x2 = int((-valueB - math.sqrt(delta)) / (2*valueA))                                                  
+        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2}")

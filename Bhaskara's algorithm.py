4303c4849238bfc026d52545a828cadf7716f05a
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2023-05-15 19:47:23 -0300

    Bhaskara's algorithm.py

--- Bhaskara's algorithm.py
+++ Bhaskara's algorithm.py
@@ -1,30 +1,23 @@
+#   Quadratic algorithm
 import cmath, math;
 
-def InputData ():
-    valueA = float(input("What is the value of 'a'? "))
-    valueB = float(input("What is the value of 'b'? "))
-    valueC = float(input("What is the value of 'c'? "))
-    PrintRoots(valueA, valueB, valueC)
+valueA = float(input("What is the value of 'a'? "))
+valueB = float(input("What is the value of 'b'? "))
+valueC = float(input("What is the value of 'c'? "))
 
-def delta (valueA, valueB, valueC):
-    return ((valueB**2) - (4*valueA*valueC))
+delta = (valueB**2) - (4*valueA*valueC)
 
-def PrintRoots(valueA, valueB, valueC):
-    resultDelta = delta(valueA, valueB, valueC)
-    if (resultDelta == 0):
-        x1 = int((-valueB + math.sqrt(resultDelta)) / (2*valueA))
-        print(f"Δ = {int(resultDelta)} \nWith the only real root being: {x1}")
+if (delta == 0):
+    x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))
+    print(f"Δ = {int(delta)} \nWith the only real root being: {x1}")
+else:
+    if (delta < 0):
+        delta = (valueB**2) - (4*valueA*valueC)
+        x1 = (-valueB + cmath.sqrt(delta)) / (2*valueA)
+        x2 = (-valueB - cmath.sqrt(delta)) / (2*valueA)
+        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2} \n(Δ < 0 there are no real roots)")
     else:
-        if (resultDelta < 0): 
-            x1 = (-valueB + cmath.sqrt(resultDelta)) / (2*valueA)
-            x2 = (-valueB - cmath.sqrt(resultDelta)) / (2*valueA)
-            print(f"Δ = {int(resultDelta)} \nWith the roots being: {x1} e {x2} \n(Δ < 0 there are no real roots)")
-        else:
-            x1 = int((-valueB + math.sqrt(resultDelta)) / (2*valueA))
-            x2 = int((-valueB - math.sqrt(resultDelta)) / (2*valueA))
-            print(f"Δ = {int(resultDelta)} \nWith the roots being: {x1} e {x2}")
-
-
-
-#    <-!->  Algorithm activation  <-!->
-InputData()
+        x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))
+        x2 = int((-valueB - math.sqrt(delta)) / (2*valueA))
+        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2}")
+        

8ed91b3499a966340290738ef644818894a7740a
Cauã Nunes <106976173+Nun3s01@users.noreply.github.com>
2023-05-15 19:54:17 -0300

    Bhaskara's algorithm update-I
    Improvement of functions, and "simplification" of the code.

--- Bhaskara's algorithm.py
+++ Bhaskara's algorithm.py
@@ -1,23 +1,30 @@
-#   Quadratic algorithm
 import cmath, math;
 
-valueA = float(input("What is the value of 'a'? "))
-valueB = float(input("What is the value of 'b'? "))
-valueC = float(input("What is the value of 'c'? "))
+def InputData ():
+    valueA = float(input("What is the value of 'a'? "))
+    valueB = float(input("What is the value of 'b'? "))
+    valueC = float(input("What is the value of 'c'? "))
+    PrintRoots(valueA, valueB, valueC)
 
-delta = (valueB**2) - (4*valueA*valueC)
+def delta (valueA, valueB, valueC):
+    return ((valueB**2) - (4*valueA*valueC))
 
-if (delta == 0):
-    x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))
-    print(f"Δ = {int(delta)} \nWith the only real root being: {x1}")
-else:
-    if (delta < 0):
-        delta = (valueB**2) - (4*valueA*valueC)
-        x1 = (-valueB + cmath.sqrt(delta)) / (2*valueA)
-        x2 = (-valueB - cmath.sqrt(delta)) / (2*valueA)
-        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2} \n(Δ < 0 there are no real roots)")
+def PrintRoots(valueA, valueB, valueC):
+    resultDelta = delta(valueA, valueB, valueC)
+    if (resultDelta == 0):
+        x1 = int((-valueB + math.sqrt(resultDelta)) / (2*valueA))
+        print(f"Δ = {int(resultDelta)} \nWith the only real root being: {x1}")
     else:
-        x1 = int((-valueB + math.sqrt(delta)) / (2*valueA))
-        x2 = int((-valueB - math.sqrt(delta)) / (2*valueA))
-        print(f"Δ = {int(delta)} \nWith the roots being: {x1} e {x2}")
-        
+        if (resultDelta < 0): 
+            x1 = (-valueB + cmath.sqrt(resultDelta)) / (2*valueA)
+            x2 = (-valueB - cmath.sqrt(resultDelta)) / (2*valueA)
+            print(f"Δ = {int(resultDelta)} \nWith the roots being: {x1} e {x2} \n(Δ < 0 there are no real roots)")
+        else:
+            x1 = int((-valueB + math.sqrt(resultDelta)) / (2*valueA))
+            x2 = int((-valueB - math.sqrt(resultDelta)) / (2*valueA))
+            print(f"Δ = {int(resultDelta)} \nWith the roots being: {x1} e {x2}")
+
+
+
+#    <-!->  Algorithm activation  <-!->
+InputData()
